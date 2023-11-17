b299fba6f53c843ae95b9157c25c255c41ad86dd
Miledi75 <mileto.di.marco@howest.be>
2023-11-17 10:34:44 +0100

    Add Mvc project for Vue

---
+++ cu.ApiVue.Lesvoorbeeld.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using cu.ApiVue.Lesvoorbeeld.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace cu.ApiVue.Lesvoorbeeld.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ cu.ApiVue.Lesvoorbeeld.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cu.ApiVue.Lesvoorbeeld.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
